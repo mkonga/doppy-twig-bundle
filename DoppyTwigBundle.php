39056380039807e33ae9530070b4f85f97a50386
Daan van den Berg <info@doppy.nl>
2016-02-11 13:09:07 +0100

    empty bundle

---
+++ DoppyTwigBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Doppy\TwigBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DoppyTwigBundle extends Bundle
+{
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Doppy Twig Bundle
+
+A Symfony2/Symfony3 bundle containing some functionality that might be useful.
